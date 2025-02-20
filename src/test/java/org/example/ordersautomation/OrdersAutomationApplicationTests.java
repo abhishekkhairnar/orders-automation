ecfd35a3a94a018ec4ea62a562386b7b23ce71b2
abhishekkhairnar <abhishek.khairnar@noovosoft.com>
2025-02-20 23:18:54 +0530

    initial commit

---
+++ src/test/java/org/example/ordersautomation/OrdersAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ordersautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
